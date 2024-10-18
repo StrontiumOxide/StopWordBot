b9ce89f7a7fd6fe53c50d787fa1873c67f95a2ad
Dorofeev Denis <dorofeevdenis2002@outlook.com>
2024-10-18 19:59:50 +0300

    alpha 1.0.1

--- main_logic_bot/greetings/kb_greetings/inline.py
+++ main_logic_bot/greetings/kb_greetings/inline.py
@@ -3,7 +3,7 @@ from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 start_link = InlineKeyboardMarkup(
     inline_keyboard=[
         [
-            InlineKeyboardButton(text='Добавить в чат ⬇️', url='https://t.me/TestOxideBot?startgroup=start')
+            InlineKeyboardButton(text='Добавить в чат ⬇️', url='https://t.me/usernames_bot_user_bot?startgroup=start')
         ]
     ]
 )
